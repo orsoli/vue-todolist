99bf1a38f28274d444d9f2b5bd57907ef3a29721
orsoli <fljcoli@gmail.com>
2024-09-05 16:45:47 +0200

    finish MIlestone1

--- js/index.js
+++ js/index.js
@@ -6,26 +6,24 @@ const app = createApp({
   data() {
     return {
       title: " TODAY To do", // Title of app variables
-      item: null, // Initial variable of item
       // Create itemList
       itemList: [
-        {
-          text: "Prepare breakfast",
-          done: true,
-        },
-        {
-          text: "Check emails",
-          done: false,
-        },
-        {
-          text: "Attend online class",
-          done: false,
-        },
-        {
-          text: "Exercie",
-          done: false,
-        },
+        { text: "Prepare breakfast", done: false },
+        { text: "Check emails", done: false },
+        { text: "Attend online class", done: false },
+        { text: "Exercie", done: false },
       ],
     };
+  },
+
+  // Methods
+  methods: {
+    isDone(i) {
+      this.itemList[i].done = !this.itemList[i].done; // Mark as done 'change the value of done'
+    },
+
+    markClass(i) {
+      return this.itemList[i].done ? "text-decoration-underline" : ""; // The mark class value changed
+    },
   },
 }).mount("#app");
